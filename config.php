2ed3c6c0a897387fedf7ab1f01c6da902d7e3f4e
Faizan Ayubi <indianayubi@gmail.com>
2016-06-14 13:20:38 +0530

    Base Class with imp methods created

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('CONSUMER_KEY', '2pCXNC7tuqhbHhDkZwc71wgG0');
+define('CONSUMER_SECRET', 'MVNTKxtGLGEaHtCd2ff0OhHuFd5pby0HId1fscvgGgfvmjvmvn');
+define('USER_TOKEN', '1601152356-Ne8QYpsR0D3kjbhwG0vQM1stTM7av3q2W5EA7gK');
+define('USER_SECRET', 'Nm6woveYDioxe5HgV3VDIbpKZYZct6t9Di50NIVkaisUc');
---
+++ kayako.php
@@ -0,0 +1,98 @@
+<?php
+
+require 'tmhOAuth.php';
+require 'config.php';
+
+/**
+* kayako class to find retweers and # search with twitter
+*/
+class Kayako {
+
+	public $twitter;
+	public $http_code;
+	
+	function __construct() {
+		$this->twitter = new tmhOAuth(array(
+			'consumer_key' => CONSUMER_KEY,
+			'consumer_secret' => CONSUMER_SECRET,
+			'user_token' => USER_TOKEN,
+			'user_secret' => USER_SECRET
+		));
+	}
+
+	/**
+	 * Get all tweets of a person
+	 * @return array tweets
+	 */
+	public function timeline() {
+		$parameters = [];
+		$parameters['count'] = 200;
+		$path = '1.1/statuses/user_timeline.json';
+		$this->http_code = $this->twitter->request('GET', $this->twitter->url($path), $parameters);
+		return $this->output();
+	}
+
+	/**
+	 * Get all tweets which was retweeted
+	 * @return array tweets
+	 */
+	public function retweets() {
+		$tweets = json_decode($this->timeline());$results = [];
+		foreach ($tweets as $t) {
+			$path = '1.1/statuses/retweets/'.$t->id.'.json';
+			$this->http_code = $this->twitter->request('GET', $this->twitter->url($path), array());
+			$arr = json_decode($this->output());
+			if (sizeof($arr) >= 1 ) {
+				$results[] = $t->text;
+			}
+		}
+		return $results;
+	}
+
+	/**
+	 * Returns tweets with the hastag as parameter
+	 * @param  string $tag hashtag of twitter
+	 * @return array      tweets of user
+	 */
+	public function hashtag($tag = "entrepeneur") {
+		$tweets = json_decode($this->timeline());
+		$tags = []; $ts = []; $results = [];
+		foreach ($tweets as $t) {
+			$path = '1.1/statuses/retweets/'.$t->id.'.json';
+			$this->http_code = $this->twitter->request('GET', $this->twitter->url($path), array());
+			$arr = json_decode($this->output());
+			$ts[$t->id] = $t->text;
+			if (sizeof($arr) >= 1 ) {
+				foreach ($arr as $a) {
+					$hashtags = $a->entities->hashtags;
+					foreach ($hashtags as $h) {
+						$tags[$t->id] = $h->text;
+					}
+				}
+			}
+		}
+		foreach ($tags as $key => $value) {
+			if ($tag == $value) {
+				$results[] = $ts[$key];
+			}
+		}
+		return $results;
+	}
+
+	/**
+	 * Result of an request sent to twitter server
+	 * @return array output from server
+	 */
+	public function output() {
+		switch ($this->http_code) {
+			case '200':
+				$response = strip_tags($this->twitter->response['response']);
+				break;
+			
+			default:
+				$response = $this->twitter->response['error'];
+				break;
+		}
+		return $response;
+	}
+}
